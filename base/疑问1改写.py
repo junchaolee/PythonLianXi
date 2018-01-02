5920210b7ac3b4eec322db6a09c4fa0c4ece57b8
junchaolee <junchaolee0721@gmail.com>
2018-01-02 17:20:36 +0800

    get best

---
+++ base/疑问1改写.py
@@ -0,0 +1,21 @@
+#coding:utf-8
+"""
+一个面试题：看下面代码会输出什么
+"""
+
+def extendList(val,list=None):
+    if list is None:
+        list=[]
+    list.append(val)
+    return list
+
+list1=extendList(10)
+print 'list1=%s'%list1
+list2=extendList(234,['a','b','c'])
+print 'list2=%s'%list2
+list3=extendList('a')
+print 'list3=%s'%list3
+
+
+
+# print 'list1=%s'%list1
