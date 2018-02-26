284e15289e7212a6e9cd5a6d4b3b3e7531f12f64
junchaolee <junchaolee0721@gmail.com>
2018-02-26 10:33:47 +0800

    New:BeautifulSoup Test

--- pymssql/test.py
+++ pymssql/test.py
@@ -1,5 +1,9 @@
 #coding:utf-8
 import pyodbc
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 conn = pyodbc.connect(
     r'DRIVER={SQL Server};'
     r'SERVER=192.168.1.167,3341;'
@@ -10,6 +14,9 @@ conn = pyodbc.connect(
 mycursor=conn.cursor()
 mycursor.execute("select top 10 * from tb_YYSAccount")
 result=mycursor.fetchall()
-print 'loginaccount\tyys\t\taacount\t\tpwd\t\t'
+
+outputMode='{0:<20}{1:{4}<10}{2:<20}{3:<60}'
+#既有英文又有中文输出还是不整齐
+print outputMode.format(u'登录账号',u'名称',u'账号',u'密码',unichr(12288))
 for i in result:
-    print i[2]+"\t\t"+i[3]+"\t\t"+i[4]+"\t\t"+i[5]
+    print outputMode.format(i[2],i[3],i[4],i[5],unichr(12288))
