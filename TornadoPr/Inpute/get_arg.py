e6e548794df60726806343a8c81beddb19f8be7d
junchaolee <junchaolee0721@gmail.com>
2018-03-11 21:49:42 +0800

    add_new

---
+++ TornadoPr/Inpute/get_arg.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+import tornado.web
+import tornado.ioloop
+import tornado.httpserver
+import tornado.options
+
+from tornado.options import define,options
+from tornado.web import RequestHandler,url
+
+tornado.options.define("port",type=int ,default=8000,help="server port")
+
+class IndexHandler(RequestHandler):
+    def get(self):
+        subject=self.get_query_argument("subject")
+        self.write(subject)
+
+if __name__=='__main__':
+    tornado.options.parse_command_line()
+    app=tornado.web.Application(
+        [(r"/",IndexHandler),
+        ],
+        debug=True
+    )
+
+    http_server=tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
